1451136c40cc1e79d90317e6fc91d4708790d439
Bernardo Sulzbach <mafagafogigante@gmail.com>
2016-06-17 17:36:21 -0300

    Computer vs. computer program should time itself

--- run-tic-tac-toe.rkt
+++ run-tic-tac-toe.rkt
@@ -3,4 +3,4 @@
 (require "tic-tac-toe.rkt")
 
 (displayln "Computer against computer...")
-(displayln (game-to-string (solve (make-empty-game))))
+(time (displayln (game-to-string (solve (make-empty-game)))))
