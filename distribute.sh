98e49fb1ef4e87480d65e87e9ff82ab37496cd6d
Bernardo Sulzbach <mafagafogigante@gmail.com>
2016-06-17 22:39:12 -0300

    Distribution script should produce a zipfile

--- distribute.sh
+++ distribute.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 #
-# Creates a standalone distribution of the game in the ./tic-tac-toe directory.
+# Creates a standalone distribution of the game in the tic-tac-toe.zip file.
 
 echo "Creating the executable..."
 raco exe game.rkt
 echo "Making a standalone distribution..."
 raco distribute tic-tac-toe game
 rm game
+echo "Making a zipfile for the distribution..."
+zip -r tic-tac-toe tic-tac-toe
+rm -rf tic-tac-toe
 echo "Done!"
